67e8e4e938c6bdafda28160645d575c34d4f1e86
IgorVVieira <igor.gutoo63@gmail.com>
2021-04-12 20:47:42 -0300

    Correções

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
                 @include('layouts.topMenu')
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Voôs agendados</h6>
+                        <h6 class="m-0 font-weight-bold text-primary text-center">Voôs agendados</h6>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
